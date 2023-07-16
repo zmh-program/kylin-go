12422f310be63e3a6bf4c672a96f29e2f1feae66
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-07-17 07:12:22 +0800

    update stream output

--- lib/stream.go
+++ lib/stream.go
@@ -40,19 +40,14 @@ func Debug[T comparable](data T) T {
 func formatStruct(s interface{}, indentLevel int) string {
 	v := reflect.ValueOf(s)
 	t := v.Type()
-	indent := strings.Repeat(" ", indentLevel*4)
-	res := ""
+	indent := strings.Repeat(" ", indentLevel)
+	res := "\n"
 
 	for i := 0; i < v.NumField(); i++ {
 		field := t.Field(i)
 		value := v.Field(i)
 
-		if value.Kind() == reflect.Struct {
-			res += fmt.Sprintf("%s%s:\n", indent, field.Name)
-			res += formatStruct(value.Interface(), indentLevel+1)
-		} else {
-			res += fmt.Sprintf("%s%s: %v\n", indent, field.Name, value.Interface())
-		}
+		res += fmt.Sprintf("%s%s: %v\n", indent, field.Name, format(value.Interface(), indentLevel+1))
 	}
 	return res
 }
@@ -60,58 +55,44 @@ func formatStruct(s interface{}, indentLevel int) string {
 func formatMap(m interface{}, indentLevel int) string {
 	v := reflect.ValueOf(m)
 	keys := v.MapKeys()
-	indent := strings.Repeat(" ", indentLevel*4)
+	indent := strings.Repeat(" ", indentLevel)
 	res := ""
 
 	for _, key := range keys {
 		value := v.MapIndex(key)
 
-		if value.Kind() == reflect.Map {
-			res += fmt.Sprintf("%s%v:\n", indent, key.Interface())
-			res += formatMap(value.Interface(), indentLevel+1)
-		} else {
-			res += fmt.Sprintf("%s%v: %v\n", indent, key.Interface(), value.Interface())
-		}
+		res += fmt.Sprintf("%s%v: %v\n", indent, format(key.Interface(), indentLevel+1), format(value.Interface(), indentLevel+1))
 	}
 	return res
 }
 
 func formatSlice(s interface{}, indentLevel int) string {
 	v := reflect.ValueOf(s)
-	indent := strings.Repeat(" ", indentLevel*4)
-	res := ""
+	indent := strings.Repeat(" ", indentLevel)
+	res := "\n"
 
 	for i := 0; i < v.Len(); i++ {
 		value := v.Index(i)
 
-		if value.Kind() == reflect.Map {
-			res += fmt.Sprintf("%s[%d]:\n", indent, i)
-			res += formatMap(value.Interface(), indentLevel+1)
-		} else if value.Kind() == reflect.Struct {
-			res += fmt.Sprintf("%s[%d]:\n", indent, i)
-			res += formatStruct(value.Interface(), indentLevel+1)
-		} else {
-			res += fmt.Sprintf("%s[%d]: %v\n", indent, i, value.Interface())
-		}
+		res += fmt.Sprintf("%s[%d]: %v\n", indent, i, format(value.Interface(), indentLevel+1))
 	}
 	return res
 }
 
 func format(v interface{}, indentLevel int) string {
-	if v == nil {
-		return "null"
-	}
+	res := ""
 
 	switch reflect.TypeOf(v).Kind() {
-	case reflect.Struct:
-		return formatStruct(v, indentLevel)
 	case reflect.Slice, reflect.Array:
-		return formatSlice(v, indentLevel)
+		res += formatSlice(v, indentLevel)
 	case reflect.Map:
-		return formatMap(v, indentLevel)
+		res += formatMap(v, indentLevel)
+	case reflect.Struct:
+		res += formatStruct(v, indentLevel)
 	default:
-		return fmt.Sprintf("%v", v)
+		res += fmt.Sprintf("%v", v)
 	}
+	return res
 }
 
 func Print(s interface{}) {
